a9edea95faf7c89fdf8703e238ba4aec8e2124d1
Sam Yeo <yeosamuel95@gmail.com>
2021-02-14 22:47:26 -0700

    Correct typo from int to integer

--- db/migrate/20210214080511_create_invoice_items.rb
+++ db/migrate/20210214080511_create_invoice_items.rb
@@ -1,7 +1,7 @@
 class CreateInvoiceItems < ActiveRecord::Migration[5.2]
   def change
     create_table :invoice_items do |t|
-      t.int :quantity
+      t.integer :quantity
       t.float :unit_price
       t.timestamps
     end
