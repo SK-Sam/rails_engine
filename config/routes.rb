7c8cfc5e3482eaeb49f9a553bc3f3047fa334357
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 01:24:22 -0700

    Add merchant show path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :merchants, only: :show
       namespace :merchants do
         resources :search, only: :index
       end
