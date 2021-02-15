b9108d6382c8f41b44c5a814fda6bd32c3016b3c
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 00:05:15 -0700

    Add relationships and validations to Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ApplicationRecord
-
+  validates :first_name, presence: true
+  validates_presence_of :last_name
+  has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
 end
