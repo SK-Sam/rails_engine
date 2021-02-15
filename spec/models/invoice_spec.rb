194a47c7feb616bd042e9159ec417386ef6e91a8
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 01:01:27 -0700

    Add relationship tests for invoice

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Invoice do
   describe 'relationships' do
-    
-  end
-  describe 'validations' do
-    
+    it { should belong_to :customer }
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many :transactions }
+    it { should have_many(:items).through(:invoice_items) }
   end
 end
