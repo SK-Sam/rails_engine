ab3bd351e6804f590e04798f9fdb563c01599ac2
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 00:03:19 -0700

    Add relationship tests to Customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Customer do
   describe 'relationships' do
-    
+    it { should have_many :invoices }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:merchants).through(:invoices) }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:items).through(:invoice_items)}
   end
   describe 'validations' do
     
