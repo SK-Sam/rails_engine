4fa8eaa80ef56137cf081f42c554c045b3daaff0
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 19:49:40 -0700

    Create handle_pagination method which passes test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,10 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.handle_pagination(per_page=20, page=1)
+    offset = 0 if page < 2 
+    offset = per_page * (page - 1) if page >= 2
+    all.limit(per_page).offset(offset)
+  end
 end
