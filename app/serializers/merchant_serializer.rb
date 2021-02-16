e30c74ca490b3cdba16e2093fa6d1c5ea520d785
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 00:13:58 -0700

    Create serializer for items

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,10 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
+  has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+  
   attributes :name
 end
