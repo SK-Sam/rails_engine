dccb489de664ca967554886667c6f6252c17026c
Sam Yeo <yeosamuel95@gmail.com>
2021-02-15 20:17:16 -0700

    Update handle_pagination with creation of helper method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,9 +8,15 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def self.handle_pagination(per_page=20, page=1)
-    offset = 0 if page < 2 
-    offset = per_page * (page - 1) if page >= 2
-    per_page = 20 if per_page > 20
-    all.limit(per_page).offset(offset)
+    page_adjustments = handle_sad_inputs(per_page, page)
+    limit = page_adjustments[:per_page]
+    offset = page_adjustments[:per_page] * (page_adjustments[:page] - 1)
+    all.limit(limit).offset(offset)
+  end
+
+  def self.handle_sad_inputs(per_page, page)
+    per_page = 20 if per_page == nil || per_page > 20 || per_page < 1
+    page = 1 if page == nil || page < 2
+    { per_page: per_page, page: page }
   end
 end
