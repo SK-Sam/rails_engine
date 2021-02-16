14ece1046f9415037a7c7f0ba63e8f9209389562
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 00:17:44 -0700

    Remove additional relationships in ItemSerializer to pass test

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,9 +1,9 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
-  has_many :transactions, through: :invoices
+  # belongs_to :merchant
+  # has_many :invoice_items
+  # has_many :invoices, through: :invoice_items
+  # has_many :transactions, through: :invoices
 
   attributes :name, :description, :unit_price, :merchant_id
 end
