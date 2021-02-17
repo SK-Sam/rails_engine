1a8c38cbf1a12cd3180f0ae3063274182c2523cc
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 21:37:23 -0700

    Add item show test

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Items', type: :request do
-  describe 'fetching items' do
+  describe 'fetching all items' do
     it 'can fetch at most 20 items at a time' do
       merchant = create(:merchant)
       item_list = create_list(:item, 22, merchant: merchant)
@@ -24,4 +24,27 @@ RSpec.describe 'Items', type: :request do
       end
     end
   end
+  describe 'fetching a single item' do
+    it 'succeeds when there is something to fetch' do
+      merchant = create(:merchant)
+      item = create(:item, merchant: merchant)
+      expected_attributes = {
+        name: item.name,
+        description: item.description,
+        unit_price: item.unit_price,
+        merchant_id: item.merchant_id
+      }
+
+      get api_v1_item_path(item.id)
+
+      expect(response.status).to eq(200)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data][:id]).to eq(merchant.id.to_s)
+      # expect that every attribute we want up above shows up in our output
+      expected_attributes.each do |attribute, value|
+        expect(json[:data][:attributes][attribute]).to eq(value)
+      end
+    end
 end
