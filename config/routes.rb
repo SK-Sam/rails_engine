3a3abc019f4df86117eac77b1da4d4c284eb46e4
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 22:32:15 -0700

    Add route for item create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show, :create]
       resources :merchants, only: [:index, :show] do
         scope module: 'merchants' do
           resources :items, only: :index
