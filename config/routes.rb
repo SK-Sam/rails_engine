5d218a29a37d02c5af84664ea1264022cc924f6d
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 20:20:04 -0700

    Add route for items index action, create empty controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :items, only: :index
       resources :merchants, only: [:index, :show] do
         scope module: 'merchants' do
           resources :items, only: :index
         end
-      end
-      namespace :merchants do
-        resources :search, only: :index
       end
     end
   end
