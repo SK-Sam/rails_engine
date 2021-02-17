a02aa70772b8470a955f92a9266a9918913efb9f
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 20:30:45 -0700

    Add to_i to handle_pagination args

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    @items = Item.handle_pagination(params[:per_page], params[:page])
+    @items = Item.handle_pagination(params[:per_page].to_i, params[:page].to_i)
     render json: ItemSerializer.new(@items)
   end
 end
