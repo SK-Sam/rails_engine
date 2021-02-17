9f79cf4946bc894b13e1633f83c52eb5749cab38
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 21:39:45 -0700

    Add show method to items controller which passes test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ItemsController < ApplicationController
     @items = Item.handle_pagination(params[:per_page].to_i, params[:page].to_i)
     render json: ItemSerializer.new(@items)
   end
+
+  def show
+    @item = Item.find(params[:id])
+    render json: ItemSerializer.new(@item)
+  end
+
 end
