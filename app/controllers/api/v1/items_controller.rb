6849224e2e544d23f2ef93fbb5bf3d2faadf1dd7
Sam Yeo <yeosamuel95@gmail.com>
2021-02-16 20:25:10 -0700

    Add index action to pass test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
-  
+  def index
+    @items = Item.handle_pagination(params[:per_page], params[:page])
+    render json: ItemSerializer.new(@items)
+  end
 end
