868d30e9f157a79ace941c766f92452bd5f3afaf
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 15:03:17 -0700

    Add find_all method which passes happy path test

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::Items::SearchController < ApplicationController
     min_price = params[:min_price]
     max_price = params[:max_price]
     name = params[:name]
-    @item = Item.find_by_args(min_price, max_price, name)
-    render json: ItemSerializer.new(@item)
+    
+    @items = Item.find_by_args(min_price, max_price, name)
+    render json: ItemSerializer.new(@items)
   end
 end
