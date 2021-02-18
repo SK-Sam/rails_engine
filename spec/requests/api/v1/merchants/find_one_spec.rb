819739f8d696bef6132e46176060a93b74cfe4a8
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 00:44:52 -0700

    Create test for Merchant find one request

---
+++ spec/requests/api/v1/merchants/find_one_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Find One', type: :request do
+  describe 'fetching a merchant' do
+    it 'can fetch a specific merchant based on name' do
+      incorrect_merchant = create(:merchant, name: "Turing")
+      correct_merchant = create(:merchant, name: "Ring World")
+
+      expected_attributes = {
+        name: correct_merchant.name
+      }
+
+      get '/api/v1/merchants/find_one?name=Ring'
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expected_attributes.each do |attribute, value|
+        expect(json[:data][:attributes][:attribute]).to eq(value)
+      end
+    end
+  end
+end
