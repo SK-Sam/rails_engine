44f1f59c9a72eda10dedecd7a55daf0534cce487
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 15:00:08 -0700

    Add class method test for Item.find_by_args

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Item do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price}
   end
+  describe 'class methods' do
+    it '#find_by_args' do
+      merchant = create(:merchant)
+      item_1 = create(:item, merchant: merchant, unit_price: 50.00)
+      item_2 = create(:item, merchant: merchant, unit_price: 25.00)
+      item_3 = create(:item, merchant: merchant, unit_price: 1.00)
+
+      item_name_to_find_by = item_1.name[0..3]
+      item_price_min_to_find_by = 25.00
+      item_price_max_to_find_by = 24
+
+      expect(Item.find_by_args(nil, nil, item_name_to_find_by)).to eq([item_1])
+      expect(Item.find_by_args(item_price_min_to_find_by, nil, nil)).to eq([item_1, item_2])
+      expect(Item.find_by_args(nil, item_price_max_to_find_by, nil)).to eq([item_3])
+      expect(Item.find_by_args(item_1.unit_price, item_1.unit_price, item_1.name.upcase)).to eq([item_1])
+      expect(Item.find_by_args(nil, nil, nil)).to eq([item_1, item_2, item_3])
+      expect(Item.find_by_args(Float::MAX, Float::MAX, "20")).to eq([])
+    end
+  end
 end
