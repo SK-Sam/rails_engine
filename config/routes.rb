2fcd29d4080af889558ca1e6371c7f8400734fc7
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 00:12:16 -0700

    Add route for item's merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        scope module: 'items' do
+          resources :merchant, only: :index
+        end
+      end
       resources :merchants, only: [:index, :show] do
         scope module: 'merchants' do
           resources :items, only: :index
