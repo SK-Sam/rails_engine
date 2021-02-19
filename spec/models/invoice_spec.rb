9f886d48362dfbbf800678044489ae96739344f5
Sam Yeo <yeosamuel95@gmail.com>
2021-02-19 02:16:56 -0700

    Create class method test for Invoices get_unshipped_revenues

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe Invoice do
     it { should have_many :transactions }
     it { should have_many(:items).through(:invoice_items) }
   end
+  describe 'class methods' do
+    it '#get_unshipped_revenues' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      item_1 = create(:item, merchant: merchant_1)
+      item_2 = create(:item, merchant: merchant_2)
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: "packaged")
+      invoice_2 = create(:invoice, merchant: merchant_2, customer: customer_2)
+      transaction_1 = create(:transaction, invoice: invoice_1, result: "success")
+      transaction_2 = create(:transaction, invoice: invoice_2, result: "success")
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1, quantity: 99, unit_price: 5)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_2, quantity: 100, unit_price: 2)
+      invoice_item_3 = create(:invoice_item, invoice: invoice_2, item: item_2, quantity: 1, unit_price: 2)
+
+      expected_potential_revenue = 495.00
+
+      expect(Invoice.get_unshipped_revenues(2).first.revenue).to eq(expected_potential_revenue)
+      expect(Invoice.get_unshipped_revenues(2).count).to eq(1)
+    end
+  end
 end
