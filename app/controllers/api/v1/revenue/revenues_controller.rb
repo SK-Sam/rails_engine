d379a22ac110c1937ac3b481fde364898f587766
Sam Yeo <yeosamuel95@gmail.com>
2021-02-19 04:13:54 -0700

    Create tests for sad paths with missing quantity params and logic to pass test

--- app/controllers/api/v1/revenue/revenues_controller.rb
+++ app/controllers/api/v1/revenue/revenues_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::Revenue::RevenuesController < ApplicationController
   def most_revenue
     @merchants = Merchant.most_revenue(params[:quantity])
-    render json: {
-      error: "Quantity greater than 0 required"
-    }, status: 400 if @merchants == "Error"
-    render json: MerchantNameRevenueSerializer.new(@merchants)
+    if @merchants == "Error" || params[:quantity] == nil
+      render json: {error: "Quantity greater than 0 required"}, status: 400 #if @merchants == "Error" || params[:quantity] == nil
+    else
+      render json: MerchantNameRevenueSerializer.new(@merchants)
+    end
   end
 
   def merchant_revenue
