3334a63e72312574137f0f51fbee813b18516178
Sam Yeo <yeosamuel95@gmail.com>
2021-02-19 02:31:34 -0700

    Add get_unshipped_revenues method which passes test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,14 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.get_unshipped_revenues(qty=10)
+    return "Error" if qty == nil || qty.to_i <= 0 || qty.to_s.count("a-zA-Z") > 0
+    joins(:invoice_items, :transactions)
+    .select("invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .where("invoices.status = ? AND transactions.result = ?", "packaged", "success")
+    .group("invoices.id")
+    .order("revenue DESC")
+    .limit(qty.to_i)
+  end
 end
