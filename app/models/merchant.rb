a9e2d22ea840c1a60a863dc510e74ba1b4b918d1
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 21:04:44 -0700

    Add method most_revenue which passes test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,13 @@ class Merchant < ApplicationRecord
   def self.find_by_name_alphabetical(name)
     where('LOWER(name) LIKE ?', "%#{name.downcase}%").order(name: :asc).first
   end
+
+  def self.most_revenue(qty)
+    return "Error" if qty == nil || qty <= 0
+    joins(invoices: [:transactions, :invoice_items])
+    .select("merchants.*, invoice_items.quantity * invoice_items.unit_price AS revenue")
+    .where("transactions.result = ?", "success")
+    .order("revenue DESC")
+    .limit(qty)
+  end
 end
