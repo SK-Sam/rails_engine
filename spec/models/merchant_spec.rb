7b1a679c0150e47b9c9a09d6a49482ab2c70e41f
Sam Yeo <yeosamuel95@gmail.com>
2021-02-18 23:24:21 -0700

    Add test for class method sold_most_items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -62,5 +62,27 @@ RSpec.describe Merchant do
       expect(Merchant.most_revenue(0)).to eq("Error")
       expect(Merchant.most_revenue(nil)).to eq("Error")
     end
+    it '#sold_most_items' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      item_1 = create(:item, merchant: merchant_1)
+      item_2 = create(:item, merchant: merchant_2)
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1)
+      invoice_2 = create(:invoice, merchant: merchant_2, customer: customer_2)
+      transaction_1 = create(:transaction, invoice: invoice_1, result: "success")
+      transaction_2 = create(:transaction, invoice: invoice_2, result: "success")
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1, quantity: 99, unit_price: 5)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_2, quantity: 100, unit_price: 2)
+
+      expected_attributes = {
+        name: merchant_2.name,
+        quantity: 100
+      }
+
+      expect(Merchant.sold_most_items(2).first.name).to eq(merchant_2.name)
+      expect(Merchant.sold_most_items(2).first.count).to eq(invoice_item_2.quantity)
+    end
   end
 end
